b143cecf6605826adaa2b84486632e0e4d469159
Jinglin Tan <lkluckly009@gmail.com>
2022-11-01 02:14:09 -0400

    Add comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+// app.js is to query all needed elements, and use functions from other js files
+
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 
