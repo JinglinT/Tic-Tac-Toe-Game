0c87715a7fd6acf422f7679713cffa860206fb25
Jinglin Tan <lkluckly009@gmail.com>
2022-11-02 22:27:20 -0400

    Validate user input with JS

--- scripts/config.js
+++ scripts/config.js
@@ -6,11 +6,17 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorOutputElement.textContent = "";
 }
 
 function savePlayerconfig(event) {
   event.preventDefault(event.target);
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get("playerName");
-  console.log(enteredPlayername);
+  const enteredPlayername = formData.get("playerName").trim();
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add("error");
+    errorOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
 }
